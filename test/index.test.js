79d7861ac4a4eacb7790a67fa79973d633b59bb5
Richard Szakacs <richardszkcs@gmail.com>
2017-10-24 05:02:11 +0200

    Add tests

---
+++ test/index.test.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const tap = require('tap')
+const path = require('path')
+const pkg = require('../package')
+const getSRI = require(path.normalize(__dirname + '/../' + pkg.main))
+
+tap.test(pkg.name, t => {
+  t.test('error', t => {
+    let message
+
+    try {
+      getSRI()
+    } catch (error) {
+      message = error.message
+    }
+    t.equal(message, 'The string parameter must be a string type, got: undefined')
+
+    try {
+      getSRI(0)
+    } catch (error) {
+      message = error.message
+    }
+    t.equal(message, 'The string parameter must be a string type, got: number')
+
+    try {
+      getSRI('')
+    } catch (error) {
+      message = error.message
+    }
+    t.equal(message, 'The string parameter must have a .length > 0')
+
+    t.end()
+  })
+
+  t.test('algorithm', t => {
+    let hash
+
+    hash = getSRI('-', getSRI.SHA256, true)
+    t.equal(hash.indexOf('sha256-'), 0)
+
+    hash = getSRI('-', getSRI.SHA384, true)
+    t.equal(hash.indexOf('sha384-'), 0)
+
+    hash = getSRI('-', getSRI.SHA512, true)
+    t.equal(hash.indexOf('sha512-'), 0)
+
+    hash = getSRI('-', 'no-hash', true)
+    t.equal(hash.indexOf('sha256-'), 0)
+
+    t.end()
+  })
+
+  t.test('prefix', t => {
+    t.equal(getSRI('-', getSRI.SHA256, false), 'OXPgIukyIPkhLBjQ0MVDrnwwnkZkDak6SgMU3pmfURI=')
+    t.equal(getSRI('-', getSRI.SHA256, true), 'sha256-OXPgIukyIPkhLBjQ0MVDrnwwnkZkDak6SgMU3pmfURI=')
+
+    t.end()
+  })
+
+  t.end()
+})
