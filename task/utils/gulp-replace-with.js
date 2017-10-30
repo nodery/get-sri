f742653d95a3d1ab2939e902e2fd91787f736ff9
Richard Szakacs <richardszkcs@gmail.com>
2017-10-30 05:43:08 +0100

    Refactor and simplify plugin

--- task/utils/gulp-replace-with.js
+++ task/utils/gulp-replace-with.js
@@ -1,50 +1,48 @@
 'use strict'
 
-const tap = require('gulp-tap')
-const replaceWith = require('./replace-with')
+const path = require('path')
 
-/**
- * @param {Object} file
- * @param {Array}  args
- */
-function replacer (file, args) {
-  var content = file.contents.toString()
+const through = require('through2')
+const PluginError = require('plugin-error')
+const isBinary = require('file-is-binary')
+const replaceWithFn = require('./replace-with')
 
-  args = Array.prototype.slice.call(args)
+const PLUGIN_NAME = 'gulp-replace-with'
 
-  args.forEach(function (entry) {
-    if (typeof entry === 'function') {
-      content = replaceWithFunction(content, entry)
-    } else {
-      content = replaceWithKeyValue(content, entry)
+function gulpReplaceWith (searchFor, replaceWith, replaceAll) {
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file)
+      return
     }
+
+    if (file.isStream()) {
+      cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'))
+      return
+    }
+
+    if (isBinary(file)) {
+      const fileName = path.basename(file.path)
+
+      cb(new PluginError(PLUGIN_NAME, 'File "' + fileName + '" on "' + file.path + '" must be a text file'))
+      return
+    }
+
+    try {
+      let content = file.contents.toString()
+
+      content = typeof searchFor === 'function'
+        ? searchFor(content)
+        : replaceWithFn(content, searchFor, replaceWith, replaceAll)
+
+      file.contents = Buffer.from(content)
+      this.push(file)
+    } catch (error) {
+      this.emit('error', new PluginError(PLUGIN_NAME, error, {fileName: file.path}))
+    }
+
+    cb()
   })
-
-  file.contents = Buffer.from(content)
 }
 
-/**
- * Replaces any key-value matches in the content of the file.
- *
- * @param {string} content     - The content of the file in the Gulp stream to modify.
- * @param {Object} keyValueMap - The key-value map to us to replace the file content.
- *
- * @returns {string} The modified content of the file
- */
-function replaceWithKeyValue (content, keyValueMap) {
-  return replaceWith(content, keyValueMap)
-}
-
-/**
- * Replaces the content of the file by using a custom callback.
- *
- * @param {string}   content  - The content of the file in the Gulp stream to modify.
- * @param {Function} callback - The callback, which result is used to replace the file content.
- *
- * @returns {string} The modified content of the file.
- */
-function replaceWithFunction (content, callback) {
-  return callback(content)
-}
-
-module.exports = function () { return tap(file => replacer(file, arguments)) }
+module.exports = gulpReplaceWith
