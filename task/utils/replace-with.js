2a2b47e9b6574d3cc9a5b3c1c8d4e1499eb75d54
Richard Szakacs <richardszkcs@gmail.com>
2017-10-30 05:32:11 +0100

    Add several argument type support

--- task/utils/replace-with.js
+++ task/utils/replace-with.js
@@ -3,22 +3,76 @@
 /**
  * Replace the given string containing placeholder with the key-value pairs of the object.
  *
- * @param {string} string - The string to use to replace the placeholders within.
- * @param {Object} object - The key-value pairs to use to replace the placeholders with.
+ * @function replaceWith
  *
- * @returns {string}
+ * @param {string} string      - The string to use to replace the placeholders within.
+ * @param {Object} replacement - The string to use to replace the placeholders within.
+ * @param {Object} options     - The string to use to replace the placeholders within.
+ *
+ * @returns {string} The replaced string.
+ *//**
+ * Replace the given string containing placeholder with the key-value pairs of the object.
+ *
+ * @function replaceWith
+ *
+ * @param {string}   string   - The string to use to replace the placeholders within.
+ * @param {Function} replacer - The string to use to replace the placeholders within.
+ * @param {Object}   options  - The string to use to replace the placeholders within.
+ *
+ * @returns {string} The replaced string.
+ *//**
+ * Replace the given string containing placeholder with the key-value pairs of the object.
+ *
+ * @function replaceWith
+ *
+ * @param {string} string      - The string to use to replace the placeholders within.
+ * @param {RegExp} searchFor   - The string to use to replace the placeholders within.
+ * @param {string} replaceWith - The key-value pairs to use to replace the placeholders with.
+ *
+ * @returns {string} The replaced string.
+ *//**
+ * Replace the given string containing placeholder with the key-value pairs of the object.
+ *
+ * @function replaceWith
+ *
+ * @param {string}  string            - The string to use to replace the placeholders within.
+ * @param {string}  searchFor         - The string to use to replace the placeholders within.
+ * @param {string}  replaceWith       - The key-value pairs to use to replace the placeholders with.
+ * @param {boolean} [replaceAll=true] - The key-value pairs to use to replace the placeholders with.
+ *
+ * @returns {string} The replaced string.
  */
-function replaceWith (string, object) {
-  string = string || ''
-  object = object || {}
+function replaceWith (string, searchFor, replaceWith, replaceAll) {
+  // TODO: string as replacement, function as replacement, replaceAll as 3rd boolean parameter
+  // TODO: set beginning/end replacement characters
+  // TODO: replace patterns: {{ pattern }}, { pattern }, [ pattern ], [[ pattern ]], pattern._-pattern
 
-  return string.replace(/{{\s*?(\w+)\s*?}}/g, function (match, key) {
-    if (key in object) {
-      return object[key]
-    }
+  if (typeof searchFor === 'string') {
+    replaceAll = typeof replaceAll === 'boolean' ? replaceAll : true
 
-    return match
-  })
+    return replaceAll
+      ? string.split(searchFor).join(replaceWith)
+      : string.replace(searchFor, replaceWith)
+  }
+
+  if (searchFor instanceof RegExp) {
+    // TODO: note in the document, that replaceAll is ignored in favor of the RegExp /g flag
+    return string.replace(searchFor, replaceWith)
+  }
+
+  if (Object.prototype.toString.call(searchFor) === '[object Object]') {
+    return string.replace(/{{\s*?(\w+)\s*?}}/g, function (match, key) {
+      return key in searchFor
+        ? searchFor[key]
+        : match
+    })
+  }
+
+  if (typeof searchFor === 'function') {
+    return string.replace(/{{\s*?(\w+)\s*?}}/g, searchFor)
+  }
+
+  return string
 }
 
 module.exports = replaceWith
