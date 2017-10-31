73af3d092b419c07c28691699985727033a0102a
Richard Szakacs <richardszkcs@gmail.com>
2017-10-31 05:01:57 +0100

    Add real-world examples as fixtures and tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,6 @@
 const tap = require('tap')
 const path = require('path')
+const fs = require('fs')
 const pkg = require('../package')
 const getSRI = require(path.normalize(path.join(__dirname, '../', pkg.main)))
 
@@ -53,6 +54,32 @@ tap.test(pkg.name, function (t) {
     t.equal(getSRI('-', getSRI.SHA256), 'OXPgIukyIPkhLBjQ0MVDrnwwnkZkDak6SgMU3pmfURI=')
     t.equal(getSRI('-', getSRI.SHA256, false), 'OXPgIukyIPkhLBjQ0MVDrnwwnkZkDak6SgMU3pmfURI=')
     t.equal(getSRI('-', getSRI.SHA256, true), 'sha256-OXPgIukyIPkhLBjQ0MVDrnwwnkZkDak6SgMU3pmfURI=')
+
+    t.end()
+  })
+
+  t.test('files', function (t) {
+    const fixtures = path.normalize(path.join(process.cwd(), '/test/fixtures/'))
+    const files = [
+      'bootstrap.min.css',
+      'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+
+      'bootstrap.min.js',
+      'sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy',
+
+      'jquery-3.3.1.slim.min.js',
+      'sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo',
+
+      'popper.min.js',
+      'sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49'
+    ]
+
+    for (let i = 0, len = files.length; i < len; i += 2) {
+      const content = fs.readFileSync(path.join(fixtures, files[i])).toString()
+      const hash = files[i + 1]
+
+      t.equal(getSRI(content, getSRI.SHA384, true), hash)
+    }
 
     t.end()
   })
